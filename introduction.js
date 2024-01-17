22a2adee1eb1e3c1005832786347a2705eae2af0
Edward Taylor <edwardtaylor@Edwards-MacBook-Pro.local>
2024-01-17 20:00:23 +0000

    v change

--- introduction.js
+++ introduction.js
@@ -1,66 +1,66 @@
 function getURLParams() {
-    const params = new URLSearchParams(window.location.search);
-    console.log(params)
-    return params;
+  const params = new URLSearchParams(window.location.search);
+  console.log(params)
+  return params;
 }
 
 // Function to set iframe source
 function setIframeSrc() {
-    const iframe = document.getElementById('frameworkFrame');
-    const params = getURLParams();
+  const iframe = document.getElementById('frameworkFrame');
+  const params = getURLParams();
 
-    let iframeURL = null;
-    if (!!params && params.get("public") == "true") {
-        iframeURL = "https://script.google.com/macros/s/AKfycbzh2NETgnKgirMcx8jZeG1WPIZNCU9MNNOdC8MTuxTUa-VjMPxhHYxW4ZHAsCyfJek/exec";
-        console.log("public")
-        console.log("AKfycbzh2NETgnKgirMcx8jZeG1WPIZNCU9MNNOdC8MTuxTUa-VjMPxhHYxW4ZHAsCyfJek")
-    } else {
-        console.log("private")
-        console.log("AKfycbwDvtNcAn9Wb4PD_CR0ahUCb6sTVCgrBovnsUeX-oDZjIelgJ5Fdu9J7XmUBNNpIzU")
-        iframeURL = 'https://script.google.com/macros/s/AKfycbwDvtNcAn9Wb4PD_CR0ahUCb6sTVCgrBovnsUeX-oDZjIelgJ5Fdu9J7XmUBNNpIzU/exec';
-    }
+  let iframeURL = null;
+  if (!!params && params.get("public") == "true") {
+      iframeURL = "https://script.google.com/macros/s/AKfycbyrEC71RIuYqBhNCYy2smwlihkT4nwFPFnbCPuSkkjaHSQOcasMBP7H6yIa-33CVZc/exec";
+      console.log("public")
+      console.log("AKfycbyrEC71RIuYqBhNCYy2smwlihkT4nwFPFnbCPuSkkjaHSQOcasMBP7H6yIa-33CVZc")
+  } else {
+      console.log("private")
+      console.log("AKfycbyHkmXDxBgbBJlGztMY19RLTNBQjgaORFeTM2aeBj_G7MmbbLW95sMVQKbdsG7JIwo")
+      iframeURL = 'https://script.google.com/macros/s/AKfycbyHkmXDxBgbBJlGztMY19RLTNBQjgaORFeTM2aeBj_G7MmbbLW95sMVQKbdsG7JIwo/exec';
+  }
 
-    // Append parameters to iframe URL
-    iframe.src = `${iframeURL}?${params}`;
+  // Append parameters to iframe URL
+  iframe.src = `${iframeURL}?${params}`;
 }
 
 function setPopup() {
-    const params = getURLParams();
-    if (!!params && params.get("tt")=="true") {
-        showPopup("public_popup_box");
-        let title = params.get("dt");
-        let titleDiv = document.getElementById("popup_title");
-        titleDiv.innerHTML = title;
-    } else if (!!params && params.get("nv")=="true") {
-        showPopup("private_popup_box");
-    }
+  const params = getURLParams();
+  if (!!params && params.get("tt")=="true") {
+      showPopup("public_popup_box");
+      let title = params.get("dt");
+      let titleDiv = document.getElementById("popup_title");
+      titleDiv.innerHTML = title;
+  } else if (!!params && params.get("nv")=="true") {
+      showPopup("private_popup_box");
+  }
 }
 
 function showPopup(popupId) {
-    let popupBg = document.getElementById("popup_background");
-    let popupBox = document.getElementById(popupId);
-    popupBg.classList.remove('hidden');
-    popupBox.classList.remove('hidden');
+  let popupBg = document.getElementById("popup_background");
+  let popupBox = document.getElementById(popupId);
+  popupBg.classList.remove('hidden');
+  popupBox.classList.remove('hidden');
 }
 
 function hidePopups() {
-    let popupBg = document.getElementById("popup_background");
-    let publicPopupBox = document.getElementById("public_popup_box");
-    let privatePopupBox = document.getElementById("private_popup_box");
-    let cta = document.getElementById("continue_cta");
-    popupBg.classList.add('hidden');
-    privatePopupBox.classList.add('hidden');
-    publicPopupBox.classList.add('hidden');
+  let popupBg = document.getElementById("popup_background");
+  let publicPopupBox = document.getElementById("public_popup_box");
+  let privatePopupBox = document.getElementById("private_popup_box");
+  let cta = document.getElementById("continue_cta");
+  popupBg.classList.add('hidden');
+  privatePopupBox.classList.add('hidden');
+  publicPopupBox.classList.add('hidden');
 
-    const params = getURLParams();
-    if (!!params && params.get("tt")=="true") {
-        let cta = document.getElementById("continue_cta");
-        cta.classList.remove('hidden');
-    }
+  const params = getURLParams();
+  if (!!params && params.get("tt")=="true") {
+      let cta = document.getElementById("continue_cta");
+      cta.classList.remove('hidden');
+  }
 }
 
 // Call the function on page load
 setIframeSrc();
 setTimeout(function() {
-    setPopup();
+  setPopup();
 },2000);
